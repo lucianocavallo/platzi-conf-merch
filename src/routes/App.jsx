b01facab9bb9cc7dcc179746af52554972dd80bd
luchx88 <lucianocavallo@gmail.com>
2021-12-21 02:23:02 -0300

    maquetacion y estructuracion de Home, Products y Product

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../styles/components/App.css"
 
 import Home from "../containers/Home";
+import Layout from "../components/Layout";
 import Checkout from "../containers/Checkout";
 import Information from "../containers/Information";
 import Payment from "../containers/Payment";
@@ -11,14 +13,16 @@ import NotFound from "../containers/NotFound";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkout/information" element={<Information />} />
-        <Route path="/checkout/payment" element={<Payment />} />
-        <Route path="/checkout/success" element={<Success />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/information" element={<Information />} />
+          <Route path="/checkout/payment" element={<Payment />} />
+          <Route path="/checkout/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
