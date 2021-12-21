226c737982dc3ddb5434f0a0a1285f89bb1a802d
luchx88 <lucianocavallo@gmail.com>
2021-12-20 23:35:45 -0300

    eslint y prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "../styles/App.css"
+import React from 'react';
+
+const hola = 'hola'
+
+import '../styles/App.css';
 
 const App = () => {
-  return (
-    <h1>Hola Mundo</h1>
-  );
-}
+  return <h1>Hola Mundo</h1>;
+};
 
 export default App;
