9c8d3b507d47e72ff304591701239fa9e7f2c124
luchx88 <lucianocavallo@gmail.com>
2021-12-21 03:39:52 -0300

    Maquetacion y estilos del flujo de pago. Integracion de iconos y conexion con react router. Custom hook y useContext.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import "../styles/components/Header.css"
+import { Link } from "react-router-dom";
+import "../styles/components/Header.css";
 
 const Header = ({children}) => {
   return (
     <div className="Header">
-      <h1 className="Header-title">PlatziConf Merch</h1>
+      <h1 className="Header-title">
+        <Link to="/">
+          PlatziConf Merch
+        </Link>
+      </h1>
       <div className="Header-checkout">
-        Checkout
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket fa-2x" />
+        </Link>
       </div>
     </div>
   );
